75389d3d2b27a7c6d4b6b6a75e478fbcd7ee4446
Aleksei Petrov <lajos@Oi-MacBook.home>
2024-05-26 19:16:23 +0200

    Build method

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -4,5 +4,18 @@ require_relative "hexlet_code/version"
 
 module HexletCode
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Tag
+    def self.build(name, attrs)
+      attributes = []
+      attrs.each do |k, v|
+        attributes << "#{k}='#{v}'"
+      end
+      if %w[br image input].include? name
+        "<#{name} #{attributes.join(" ")} />"
+      else
+        "<#{name}#{attributes.join(" ")}>#{yield if block_given?}</#{name}>"
+      end
+    end
+  end
 end
